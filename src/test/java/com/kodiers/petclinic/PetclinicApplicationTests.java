4406804a0b001840c133a9ea53b112d7407c4cd2
Victor Yamchinov <kodiers@gmail.com>
2020-06-11 01:20:30 +0300

    Initial commit

---
+++ src/test/java/com/kodiers/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodiers.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
