3f6a8131f514563a1da16103a6f2bc9a32f265f5
Victor Yamchinov <kodiers@gmail.com>
2020-06-24 19:18:50 +0300

    added interfaces

---
+++ petclinic-data/src/main/java/com/kodiers/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.kodiers.petclinic.services;
+
+import com.kodiers.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
