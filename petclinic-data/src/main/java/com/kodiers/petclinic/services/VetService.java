ee05585c44a16980b73758c39d84a58abc2428de
Victor Yamchinov <kodiers@gmail.com>
2020-06-24 20:36:58 +0300

    refactored to use CrudService

--- petclinic-data/src/main/java/com/kodiers/petclinic/services/VetService.java
+++ petclinic-data/src/main/java/com/kodiers/petclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.kodiers.petclinic.services;
 
 import com.kodiers.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
