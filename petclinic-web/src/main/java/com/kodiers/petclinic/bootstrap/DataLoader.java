efb8c715eef00ed452992cb62a81c6827271f9a3
Victor Yamchinov <kodiers@gmail.com>
2020-08-11 01:16:04 +0300

    added VisitSDJpaService

--- petclinic-web/src/main/java/com/kodiers/petclinic/bootstrap/DataLoader.java
+++ petclinic-web/src/main/java/com/kodiers/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.kodiers.petclinic.bootstrap;
 
 import com.kodiers.petclinic.model.*;
-import com.kodiers.petclinic.services.OwnerService;
-import com.kodiers.petclinic.services.PetTypeService;
-import com.kodiers.petclinic.services.SpecialtyService;
-import com.kodiers.petclinic.services.VetService;
+import com.kodiers.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialtyService specialtyService) {
+                      SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -90,6 +89,12 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasPet);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasPet);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+        visitService.save(catVisit);
 
         System.out.println("Loaded owners...");
 
